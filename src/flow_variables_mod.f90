4bb987c9ce7452d028a2f9625885f0d1204c81d1
Wei Wang <wei.wang@stfc.ac.uk>
2020-10-31 22:59:34 +0000

    Added flow variables

--- src/flow_variables_mod.f90
+++ src/flow_variables_mod.f90
@@ -1,8 +1,46 @@
 module flow_variables_mod
+  use precision_mod
+  use input_mod, only: ithermo
+  implicit none
 
+  real(WP), save, allocatable, dimension(:, :, :) :: ux, uy, uz
+  real(WP), save, allocatable, dimension(:, :, :) :: gx, gy, gz
+  real(WP), save, allocatable, dimension(:, :, :) :: pre, phi
 
+  if(ithermo == 1) then
+    real(WP), save, allocatable, dimension(:, :, :) :: massEnthalpy
+    real(WP), save, allocatable, dimension(:, :, :) :: enthalpy
+    real(WP), save, allocatable, dimension(:, :, :) :: density
+    real(WP), save, allocatable, dimension(:, :, :) :: temperature
+    real(WP), save, allocatable, dimension(:, :, :) :: thermalConductivity
+  end if
 
+  public : Allocate_flow_variables
 
+contains
+  subroutine Allocate_flow_variables ()
+    use decomp_2d
 
+    call alloc_x (ux,  opt_global=.true.)
+    call alloc_x (uy,  opt_global=.true.)
+    call alloc_x (uz,  opt_global=.true.)
+
+    call alloc_x (pre, opt_global=.true.)
+    call alloc_x (phi, opt_global=.true.)
+
+    call alloc_x (gx,  opt_global=.true.)
+    call alloc_x (gy,  opt_global=.true.)
+    call alloc_x (gz,  opt_global=.true.)
+
+    if(ithermo == 1) then
+      call alloc_x (massEnthalpy,        opt_global=.true.)
+      call alloc_x (enthalpy,            opt_global=.true.)
+      call alloc_x (density,             opt_global=.true.)
+      call alloc_x (temperature,         opt_global=.true.)
+      call alloc_x (thermalConductivity, opt_global=.true.)
+
+    end if
+
+  end subroutine Allocate_flow_variables
 
 end module flow_variables_mod
